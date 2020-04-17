014f5b477cb07631ff1446b592af5da588f7ae8f
davesammut <davesammut@gmail.com>
2020-04-17 12:44:27 +0100

    @davesammut added behaviour function to ignore rendering of dependent fields.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-require('./index').start();
+const hof = require('hof');
+
+hof({
+  behaviours: [
+    require('./apps/asylum-forms-hof/behaviours/fields-filter')
+  ],
+  routes: [
+    require('./apps/asylum-forms-hof/')
+  ]
+});
