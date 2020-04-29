a2e9658cd96acc70817fa5aaa18267bf6df8536e
davesammut <davesammut@gmail.com>
2020-04-29 13:59:45 +0100

    @davesammut changed hosts and rules ingress name

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -43,3 +43,8 @@ if ! kd --timeout=5m \
   echo "[error] failed to deploy ${NAME}"
   exit 1
 fi
+
+echo "--- deploying file vault"
+kd --timeout=5m \
+   --check-interval=5s \
+  -f kube/file-vault-ingress.yml \
