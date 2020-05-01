4bab2abf792791b5a80e2213401c40f6d92dba9a
Andy Moody <moody.andy@gmail.com>
2020-05-01 14:20:03 +0100

    Only link to combined screens if joint application

--- apps/asylum-forms-hof/index.js
+++ apps/asylum-forms-hof/index.js
@@ -228,7 +228,7 @@ module.exports = {
       forks: [{
         target: '/combined-income',
           condition: {
-            field: 'partner',
+            field: 'joint',
             value: 'yes'
           }
       }]
