d4fa31bc64c8cdd55271b77e2d63bb7ca5553f6d
davesammut <davesammut@gmail.com>
2020-06-05 15:15:39 +0100

    fixed lint errors

--- behaviours/page-analytics.js
+++ behaviours/page-analytics.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const metrics = require('../lib/metrics');
-
 const client = require('prom-client');
 const registry = client.register;
 
@@ -11,12 +9,11 @@ const submitApplyFormCounter = registry.getSingleMetric('submit_apply_form');
 module.exports = superclass => class Behaviour extends superclass {
   locals(req, res) {
     req.log('info', 'analytics:' + req.sessionID + '^' + req.path);
-    
-    if(req.originalUrl.includes('apply/previously-applied')) {
+    if (req.originalUrl.includes('apply/previously-applied')) {
       startApplyFormCounter.inc();
     }
 
-    if(req.originalUrl.includes('apply/confirm')) {
+    if (req.originalUrl.includes('apply/confirm')) {
       submitApplyFormCounter.inc();
     }
     return super.locals(req, res);
