ce4b985147103d5949c4baab862dc51ac47e9bdc
davesammut <davesammut@gmail.com>
2020-06-08 15:59:48 +0100

    Added a label per page hit for a single page hit counter

--- behaviours/page-analytics.js
+++ behaviours/page-analytics.js
@@ -3,19 +3,12 @@
 const client = require('prom-client');
 const registry = client.register;
 
-const startApplyFormCounter = registry.getSingleMetric('start_apply_form');
-const submitApplyFormCounter = registry.getSingleMetric('submit_apply_form');
+const pageHitCounter = registry.getSingleMetric('ril_page_hit_count');
 
 module.exports = superclass => class Behaviour extends superclass {
   locals(req, res) {
     req.log('info', 'analytics:' + req.sessionID + '^' + req.path);
-    if (req.originalUrl.includes('apply/previously-applied')) {
-      startApplyFormCounter.inc();
-    }
-
-    if (req.originalUrl.includes('apply/confirm')) {
-      submitApplyFormCounter.inc();
-    }
+    pageHitCounter.inc({ page: req.path });
     return super.locals(req, res);
   }
 };
