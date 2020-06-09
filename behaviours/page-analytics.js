3453d163892f96384f9d71bd9a3bbdf8cc3d8eab
davesammut <davesammut@gmail.com>
2020-06-09 09:33:07 +0100

    removed counter metric. Using gauge instead.

--- behaviours/page-analytics.js
+++ behaviours/page-analytics.js
@@ -2,14 +2,11 @@
 
 const client = require('prom-client');
 const registry = client.register;
-
-const pageHitCounter = registry.getSingleMetric('ril_page_hit_count');
 const pageHitGauge = registry.getSingleMetric('ril_page_hit_gauge');
 
 module.exports = superclass => class Behaviour extends superclass {
   locals(req, res) {
     req.log('info', 'analytics:' + req.sessionID + '^' + req.path);
-    pageHitCounter.inc({ page: req.path }, 1.0);
     pageHitGauge.inc({ page: req.path }, 1.0);
     return super.locals(req, res);
   }
