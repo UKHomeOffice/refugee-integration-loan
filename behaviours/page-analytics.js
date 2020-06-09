51e42b18fc2542480689395aa90d7bd13a913002
davesammut <davesammut@gmail.com>
2020-06-09 15:36:58 +0100

    added metric for user agent data, captured only at start pages for either apply and accept forms

--- behaviours/page-analytics.js
+++ behaviours/page-analytics.js
@@ -4,12 +4,15 @@ const client = require('prom-client');
 const registry = client.register;
 const pageHitGauge = registry.getSingleMetric('ril_page_hit_gauge');
 const visitorGauge = registry.getSingleMetric('ril_visitor_gauge');
+const visitorDeviceGauge = registry.getSingleMetric('ril_visitor_device_gauge');
 
 module.exports = superclass => class Behaviour extends superclass {
   locals(req, res) {
-    req.log('info', 'analytics:' + req.sessionID + '^' + req.path);
     pageHitGauge.inc({ page: req.path }, 1.0);
     visitorGauge.inc({ user: req.sessionID, page: req.path }, 1.0);
+    if (req.path.includes('reference-number') || req.path.includes('previously-applied')) {
+      visitorDeviceGauge.inc({ device: req.headers['user-agent'] }, 1.0);
+    }
     return super.locals(req, res);
   }
 };
