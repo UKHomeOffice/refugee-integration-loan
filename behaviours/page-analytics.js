c276a469193728afbd62805f78bc5f6773a19d3d
davesammut <davesammut@gmail.com>
2020-06-09 11:35:22 +0100

    added gauge for vistor and page data

--- behaviours/page-analytics.js
+++ behaviours/page-analytics.js
@@ -3,11 +3,13 @@
 const client = require('prom-client');
 const registry = client.register;
 const pageHitGauge = registry.getSingleMetric('ril_page_hit_gauge');
+const visitorGauge = registry.getSingleMetric('ril_visitor_gauge');
 
 module.exports = superclass => class Behaviour extends superclass {
   locals(req, res) {
     req.log('info', 'analytics:' + req.sessionID + '^' + req.path);
     pageHitGauge.inc({ page: req.path }, 1.0);
+    visitorGauge.inc({ user: req.sessionID, page: req.path }, 1.0);
     return super.locals(req, res);
   }
 };
