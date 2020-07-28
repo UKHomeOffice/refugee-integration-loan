473cca5f320544cf90127d442d2c2c3b171a537d
Andy Moody <moody.andy@gmail.com>
2020-07-28 10:04:25 +0100

    remove unused email section

--- config.js
+++ config.js
@@ -6,21 +6,6 @@ module.exports = {
   DATE_FORMAT: 'YYYY-MM-DD',
   PRETTY_DATE_FORMAT: 'Do MMMM YYYY',
   dateTimeFormat: 'DD MMM YYYY HH:MM:SS ZZ',
-  email: {
-    caseworker: process.env.CASEWORKER_EMAIL || '',
-    from: process.env.FROM_ADDRESS || '',
-    replyTo: process.env.REPLY_TO || '',
-    transport: process.env.EMAIL_TRANSPORT || 'ses',
-    transportOptions: {
-      accessKeyId: process.env.AWS_USER || '',
-      secretAccessKey: process.env.AWS_PASSWORD || '',
-      region: process.env.EMAIL_REGION || '',
-      port: process.env.EMAIL_PORT || '',
-      host: process.env.EMAIL_HOST || '',
-      ignoreTLS: process.env.EMAIL_IGNORE_TLS || '',
-      secure: process.env.EMAIL_SECURE || ''
-    }
-  },
   redis: {
     password: process.env.REDIS_PASSWORD
   },
