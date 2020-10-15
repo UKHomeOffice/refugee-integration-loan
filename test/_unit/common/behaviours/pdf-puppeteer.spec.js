7c8ff5d4f1fc58e77834f2c3607187b83f37f96e
Aaron Robinson <aaronrobinson@hod083488.home>
2020-10-15 15:48:23 +0100

    Add tests for functions in pdf puppeteer

--- test/_unit/common/behaviours/pdf-puppeteer.spec.js
+++ test/_unit/common/behaviours/pdf-puppeteer.spec.js
@@ -8,8 +8,9 @@ describe.only('pdf-puppeteer', () => {
     let newPageStub;
     let setContentStub;
     let emulateMediaStub;
-    let pfdStub;
+    let pdfStub;
     let pdfPuppeteerProxy;
+    let closeStub;
 
     let puppetLaunchArgs = {
       headless: true,
@@ -22,10 +23,22 @@ describe.only('pdf-puppeteer', () => {
     const testApplication = 'apply';
 
     beforeEach(() => {
+      closeStub = sinon.stub();
+      pdfStub = sinon.stub();
+      emulateMediaStub = sinon.stub();
+      setContentStub = sinon.stub();
       newPageStub = sinon.stub();
       launchStub = sinon.stub();
 
-      launchStub.withArgs(puppetLaunchArgs).resolves({ newPage: newPageStub });
+      launchStub.withArgs(puppetLaunchArgs).resolves({
+        newPage: newPageStub,
+        close: closeStub});
+
+      newPageStub.resolves({
+        setContent: setContentStub,
+        emulateMedia: emulateMediaStub,
+        pdf: pdfStub,
+        });
 
       pdfPuppeteerProxy = proxyquire('../apps/common/behaviours/pdf-puppeteer', {
         puppeteer: {
@@ -39,14 +52,47 @@ describe.only('pdf-puppeteer', () => {
     });
 
     it('calls puppeteer.launch', async() => {
-      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication)
+      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
       launchStub.should.have.been.calledOnce.calledWithExactly(puppetLaunchArgs);
     });
 
     it('calls newPage', async() => {
-      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication)
+      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
+      // eslint-disable-next-line no-unused-expressions
       newPageStub.should.have.been.calledOnce;
     });
+
+    it('calls setContent', async() => {
+      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
+      setContentStub.should.have.been.calledOnce.calledWithExactly(testHtml, {
+        waitUntil: 'networkidle0'
+      });
+    });
+
+    it('calls emulateMedia', async() => {
+      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
+      emulateMediaStub.should.have.been.calledOnce.calledWithExactly('screen');
+    });
+
+    it('calls pdf', async() => {
+      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
+      pdfStub.should.have.been.calledOnce.calledWithExactly({
+        path: `${testDestination}/${testTempName}`,
+        format: 'A4',
+        printBackground: true
+      });
+    });
+
+    it('calls close', async() => {
+      await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
+      // eslint-disable-next-line no-unused-expressions
+      closeStub.should.have.been.calledOnce;
+    });
+
+    it('returns file', async() => {
+      const testFile = await pdfPuppeteerProxy.generate(testHtml, testDestination, testTempName, testApplication);
+      expect(testFile).to.eql(`${testDestination}/${testTempName}`);
+    })
 
   });
 });
