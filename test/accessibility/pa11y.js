456a8e59e199f8f6d0e8a27ef7cd1ef247f98e50
Alexander Swann <alex_swann@hotmail.co.uk>
2021-05-24 15:45:13 +0100

    RIL-247 - Pa11y CI - upgraded and moved dependency

---
+++ test/accessibility/pa11y.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pa11y = require('pa11y');
+const app = require('../../index');
+
+console.log(app);
+
+// app._router.stack.forEach(r => {
+//   if (r.route && r.route.path) {
+//     console.log(r.route.path);
+//   }
+// });
