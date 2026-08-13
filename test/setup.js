f7abf9f2be9773eba230fe2ff8f503c5ccf5500a
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-08-13 18:16:11 +0100

    RIL-490 remove session secret mock

--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,6 @@
 process.env.PORT = 9080;
 process.env.NODE_ENV = 'test';
 process.env.NOTIFY_KEY = 'UNIT_TEST';
-process.env.SESSION_SECRET = process.env.SESSION_SECRET || 'cea113d678b50749404504dbb3ee4d78';
 
 const reqres = require('hof').utils.reqres;
 
